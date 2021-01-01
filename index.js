ce6fd994aa51a9ff08a706c8e8ec705509bdf624
HueteFM <huetem@gmail.com>
2021-01-01 10:34:16 -0300

    pagina e servidor de pé, com ejs, nodemon e bootsrap

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("teste");
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs'); //habilita ejs
+app.use(express.static('public')); // habilita arquivos estáticos
+
+app.get("/", function (req, res) {
+    res.render('index');
+})
+
+app.listen(8080, () => { console.log("Está rodando!"); })
